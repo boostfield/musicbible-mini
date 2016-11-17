d5f2fa7ce7352406b5e1e75d2f95be812e34a040
艹羊 <779213209@qq.com>
2016-11-17 18:27:13 +0800

    use component srcorll-view to take place of  component Swiper,

--- pages/record-detail/record-detail.js
+++ pages/record-detail/record-detail.js
@@ -1,7 +1,54 @@
 var app = getApp()
 Page({
     data:{
-        id:0
+        id:0,
+      tracklist:[
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"巴赫: 6首大提琴无伴圣诞节放水淀粉当时发生的首都发生的…",
+        subTitle:"BACH:Six Suites For Sol fdsf dsf fsdf dsfsd sdfsd…"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item02",
+        subTitle:"subTitle02"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item03",
+        subTitle:"subTitle03"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item04",
+        subTitle:"subTitle04"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item05",
+        subTitle:"subTitle05"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+      {
+        imageCoverUrl:"https://github.com/CaoyangLee/BlankApp/blob/master/scence.jpg?raw=true",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+    ],
     },
     onLoad: function(options) {
     this.setData({
