c9e7d1c6033c33da9037d3ee026f10a303520f22
艹羊 <779213209@qq.com>
2016-11-21 02:42:29 +0800

    add search record result list

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,9 +1,60 @@
 var app = getApp()
 Page({
-    isShowRealSearchBar:false,
-    searchContent:"",
     data: {
-        keywords:["巴赫","贝多芬","月光","第九号交响曲","安雅·陶尔","柴可夫斯基","钢琴奏鸣曲","迈克尔·杰克逊","小提琴协奏曲","合唱交响曲","海顿","鲁道夫"]
+         isHideFooterLoading:true,
+        keyWords:["巴赫","贝多芬","月光","第九号交响曲","安雅·陶尔","柴可夫斯基","钢琴奏鸣曲","迈克尔·杰克逊","小提琴协奏曲","合唱交响曲","海顿","鲁道夫"],
+        associateWords:["巴赫1111111111111111111111111111111111111111111","巴赫222222222222","巴赫333333333","巴赫4444444","巴赫55555555"],
+        isShowRealSearchBar:false,
+        isShowAssociateWords:false,
+        isShowRecordResult:false,
+        searchContent:"",
+        searchResultArray:[
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"巴赫: 6首大提琴无伴圣诞节放水淀粉当时发生的首都发生的…",
+        subTitle:"BACH:Six Suites For Sol fdsf dsf fsdf dsfsd sdfsd…"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item02",
+        subTitle:"subTitle02"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item03",
+        subTitle:"subTitle03"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item04",
+        subTitle:"subTitle04"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item05",
+        subTitle:"subTitle05"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+      {
+        imageCoverUrl:"http://img.blog.csdn.net/20141012230011472?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvbG1qNjIzNTY1Nzkx/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",
+        title:"item06",
+        subTitle:"subTitle06"
+      },
+    ]
     },
     actionSearch:function(){
         //搜索bar
@@ -14,12 +65,16 @@ Page({
     actionCancle:function(){
         //取消搜索
          this.setData({
-           isShowRealSearchBar:false
+           isShowRealSearchBar:false,
+           isShowAssociateWords:false,
+           isShowRecordResult:false,
+           searchContent:""
         })
     },
     actionDeleteInputValue:function(){
         //删除input内容
         this.setData({
+            isShowRecordResult:false,
             searchContent:""
         })
     },
@@ -28,6 +83,7 @@ Page({
         console.log(e);
         console.log("内容 "+e.detail.value);
         this.setData({
+            isShowAssociateWords:true,
             searchContent:e.detail.value
         })
     },
@@ -36,7 +92,38 @@ Page({
         console.log("内容 "+e.currentTarget.dataset.hotkey);
         this.setData({
             isShowRealSearchBar:true,
+            isShowAssociateWords:true,
             searchContent:e.currentTarget.dataset.hotkey
         })
+    },
+    actionAssociateWordSearch:function(e){
+        //点击关联词
+        console.log("内容 "+e.currentTarget.dataset.hotkey);
+         this.setData({
+             isShowRecordResult:true,
+             isShowAssociateWords:false,
+             searchContent:e.currentTarget.dataset.hotkey
+        })
+    },
+    onPullDownRefresh:function(){
+    console.log("xia la shua xin");
+    setTimeout(this.onPullDownRefreshSuccess,2000);
+    }, 
+    onReachBottom: function () {
+        console.log("shang la shua xin");
+        this.setData({
+            isHideFooterLoading:false
+        });
+        //延迟加载
+        setTimeout(this.onBottomRefreshSucess,2000);
+    },
+    onPullDownRefreshSuccess:function(){
+        wx.stopPullDownRefresh();
+    },
+    onBottomRefreshSucess:function(){
+        console.log("wft");
+        this.setData({
+            isHideFooterLoading:true
+        });
     }
 })
